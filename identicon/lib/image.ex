afdb22b766439deb0727e25a2908159f75282d9d
0xADADA <0xadada.pub@0xadada.pub>
2017-04-27 07:54:41 -0400

    Implemented identicon

--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -3,5 +3,8 @@ defmodule Identicon.Image do
   Defines all data related to an identicon image.
   """
 
-  defstruct hex: nil, color: nil
+  defstruct hex: nil,
+    color: nil,
+    grid: nil,
+    pixel_map: nil
 end
